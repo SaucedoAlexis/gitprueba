522c253b741a260de9264ecbd291652e0350f473
Alexis Saucedo <saucedoalexism@gmail.com>
2023-03-06 19:42:39 -0300

    segundo commit

--- family/views.py
+++ family/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from family.models import Family
+
+
 # Create your views here.
+
+def listar_familia(request):
+    family = Family.objects.all()
+    return render(request, "Datos_Familia.html",{"familia": Family})
